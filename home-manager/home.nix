2f8ba684f5c8a1cf01ede0314a987010c001529d
Al Bowles <bowlesan@proton.me>
2024-05-21 23:19:18 +0000

    refactor: aliases and envvars in a module

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -13,6 +13,7 @@
     ./tmux.nix
     #./vim.nix
     ./zsh.nix
+    ./env.nix
 
     # k8s
     #./k9s.nix
@@ -38,33 +39,6 @@
     sessionPath = [
       "$HOME/code/github.com/akatch/scripts"
     ];
-    sessionVariables = {
-      GPG_TTY = "$(tty)";
-      GREP_COLORS = "mt=38;5;214:sl=:cx=:fn=38;5;236:ln=32:bn=32:se = '38;5;233'";
-      GREP_COLOR = "38;5;214";
-      EDITOR = "vim";
-      JQ_COLORS = "1;30:0;37:0;37:0;37:0;32:1;37:1;37";
-      LANG = "en_US.UTF-8";
-      LESS = "-j.5 -R -F -X";
-      LESSCHARSET = "utf-8";
-      MANWIDTH = "60";
-      MANROFFOPT = "-c";
-      NIX_CONFIG = "experimental-features = nix-command flakes";
-      QUOTING_STYLE = "literal";
-      VISUAL = "$EDITOR";
-    };
-
-    shellAliases = {
-      "e" = "vim -p";
-      "g" = "git";
-      "gr" = "grep -r";
-      "hms" = "home-manager switch --flake .#$(whoami)@$(hostname)";
-      "l" = "ls -lhG";
-      "lah" = "ls -lahG";
-      "nrs" = "sudo nixos-rebuild switch";
-      "se" = "sudo -e";
-      "ta" = "tmux attach -t";
-    };
 
     packages = with pkgs; [
       # cli

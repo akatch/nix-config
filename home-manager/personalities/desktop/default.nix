fb6cb19c27ba8e42ca01a217e7480162e06057d4
Al Bowles <bowlesan@proton.me>
2024-05-30 11:40:18 -0500

    refactor: separate perosnality for swaywm

--- home-manager/personalities/desktop/default.nix
+++ home-manager/personalities/desktop/default.nix
@@ -3,14 +3,6 @@
   ...
 }: {
   imports = [
-    # TODO import these only if x86_64-linux
-    ./dunst.nix
-    ./i3status.nix
-    ./swayidle.nix
-    ./swaylock.nix
-    #./sway.nix
-
-    # always import alacritty for this personality
     ./alacritty.nix
     ./zathura.nix
   ];
---
+++ home-manager/personalities/desktop/swaywm/default.nix
@@ -0,0 +1,9 @@
+{
+  imports = [
+    ./dunst.nix
+    ./i3status.nix
+    ./swayidle.nix
+    ./swaylock.nix
+    #./sway.nix
+  ];
+}

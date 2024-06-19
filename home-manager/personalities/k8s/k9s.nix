3d54d609daa5f01c2b5f4ec802129bd577abd51b
Al Bowles <bowlesan@proton.me>
2024-06-19 13:24:08 -0500

    fix: skin file structure

--- home-manager/personalities/k8s/k9s.nix
+++ home-manager/personalities/k8s/k9s.nix
@@ -33,38 +33,38 @@
               fgColor = "#303030";
               focusColor = "#5f8700";
             };
-          };
 
-          menu = {
-            fgColor = "#4e4e4e";
-            keyColor =  "#5faf00";
-            numKeyColor = "#ffaf00";
-          };
+            menu = {
+              fgColor = "#4e4e4e";
+              keyColor =  "#5faf00";
+              numKeyColor = "#ffaf00";
+            };
 
-          crumbs = {
-            fgColor = "#5faf00";
-            bgColor = "default";
-            activeColor = "#262626";
-          };
+            crumbs = {
+              fgColor = "#5faf00";
+              bgColor = "default";
+              activeColor = "#262626";
+            };
 
-          status = {
-            newColor = "steelblue";
-            modifyColor = "powderblue";
-            addColor = "lightskyblue";
-            errorColor = "#ff8700";
-            highlightcolor = "royalblue";
-            killColor = "magenta";
-            completedColor = "blue";
-          };
+            status = {
+              newColor = "steelblue";
+              modifyColor = "powderblue";
+              addColor = "lightskyblue";
+              errorColor = "#ff8700";
+              highlightcolor = "royalblue";
+              killColor = "magenta";
+              completedColor = "blue";
+            };
 
-          title = {
-            fgColor = "#5faf00";
-            bgColor = "default";
-            highlightColor = "skyblue";
-            counterColor = "slateblue";
-            filterColor = "default";
-          };
+            title = {
+              fgColor = "#5faf00";
+              bgColor = "default";
+              highlightColor = "skyblue";
+              counterColor = "slateblue";
+              filterColor = "default";
+            };
 
+          };
           views = {
             table = {
               fgColor = "#5faf00";
@@ -77,17 +77,17 @@
               bgColor = "default";
               sorterColor = "#faff00";
             };
-          };
 
-          yaml = {
-            keyColor = "cyan";
-            colonColor = "green";
-            valueColor = "#767676";
-          };
+            yaml = {
+              keyColor = "cyan";
+              colonColor = "green";
+              valueColor = "#767676";
+            };
 
-          logs = {
-            fgColor = "#767676";
-            bgColor = "default";
+            logs = {
+              fgColor = "#767676";
+              bgColor = "default";
+            };
           };
         };
       };

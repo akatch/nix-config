60ccc1c8a5839a6ea096b180f1baa7cc07bad6c2
Al Bowles <bowlesan@proton.me>
2024-08-25 16:16:47 +0000

    feat: Some basic firefox settings

--- home-manager/personalities/desktop/firefox/default.nix
+++ home-manager/personalities/desktop/firefox/default.nix
@@ -1,5 +1,31 @@
 {
+  pkgs,
+  ...
+}: {
   programs.firefox = {
     enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        settings = {
+          "browser.search.defaultenginename" = "DuckDuckGo";
+          "browser.search.order.1" = "DuckDuckGo";
+          "browser.search.openintab" = true;
+          "browser.urlbar.openintab" = true;
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "font.minimum-size.x-western" = 16;
+        };
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          order = [
+            "DuckDuckGo"
+            "Wikipedia"
+          ];
+        };
+      };
+    };
   };
 }

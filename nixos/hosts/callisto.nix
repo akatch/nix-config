2c935219c243c74b02fced70d0d7a88303e07760
Al Bowles <bowlesan@proton.me>
2024-09-06 02:32:25 +0000

    feat: system.stateVersion by host

--- nixos/hosts/callisto.nix
+++ nixos/hosts/callisto.nix
@@ -38,4 +38,8 @@
       User = "al";
     };
   };
+
+  # NO TOUCHING.
+  # See `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11";
 }
--- nixos/hosts/europa.nix
+++ nixos/hosts/europa.nix
@@ -1,3 +1,7 @@
 {
   networking.hostName = "europa";
+
+  # NO TOUCHING.
+  # See `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05";
 }

e22ed0994ffd9c4ae3879536b97b564bd4a72573
Al Bowles <bowlesan@proton.me>
2024-09-07 21:02:44 +0000

    feat: Tweak atuin ui

--- home-manager/personalities/cli/atuin.nix
+++ home-manager/personalities/cli/atuin.nix
@@ -4,7 +4,13 @@
     enableZshIntegration = true;
     # https://docs.atuin.sh/configuration/config/
     settings = {
+      search_mode = "prefix";
       style = "compact";
+      inline_height = 10;
+      show_help = false;
+      show_tabs = false;
+      enter_accept = true;
+      keymap_mode = "vim-insert";
     };
   };
 }

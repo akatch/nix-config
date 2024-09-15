1b491d853b5e033fee4192b065a3112177bd3043
Al Bowles <bowlesan@proton.me>
2024-09-15 20:59:48 +0000

    feat: Declarative wifi networks

--- nixos/hosts/europa.nix
+++ nixos/hosts/europa.nix
@@ -1,5 +1,13 @@
 {
-  networking.hostName = "europa";
+  networking = {
+    hostName = "europa";
+    wireless = {
+      environmentFile = "/run/secrets/wireless.env";
+      networks = {
+        WifiIsComing.psk = "@PSK_HOME@";
+      };
+    };
+  };
 
   # NO TOUCHING.
   # See `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .

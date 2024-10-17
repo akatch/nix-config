6cf459a6a184b19391052ce88b6556bbca8bb12b
Al Bowles <bowlesan@proton.me>
2024-10-16 21:06:47 -0500

    feat: Enhanced display management hotkeys

--- home-manager/hosts/europa.nix
+++ home-manager/hosts/europa.nix
@@ -28,8 +28,9 @@
     ''
       bindgesture swipe:right workspace prev
       bindgesture swipe:left workspace next
-      bindswitch lid:off exec output eDP-1 enable
-      bindsym Mod4+z output DP-2 enable; output eDP-1 disable; workspace 1 output DP-2; workspace 2 output DP-2
+      bindswitch lid:off output eDP-1 enable
+      bindsym Mod4+z output DP-2 enable; output DP-2 pos 0 2160; output eDP-1 disable; workspace 1 output DP-2; workspace 2 output DP-2
+      bindsym Mod4+Shift+z output eDP-1 enable; workspace 1 output eDP-1; workspace 2 output eDP-1
       default_border pixel 1
     '';
   };

1d53c32861cb2f91646f612384fc8f38b6a8b4b2
Al Bowles <abowles@coreweave.com>
2025-03-03 10:09:22 -0600

    feat: Some widely used nice options
    see https://blog.gitbutler.com/how-git-core-devs-configure-git/

--- home-manager/personalities/core/git.nix
+++ home-manager/personalities/core/git.nix
@@ -38,11 +38,41 @@
     };
 
     extraConfig = {
+      branch.sort = "-committerdate";
+      column.ui = "auto";
+      commit.verbose = true;
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = "prompt";
+      init.defaultBranch = "main";
       pull = {
         rebase = true;
         autoStash = true;
       };
-      commit.verbose = true;
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      tag.sort = "version:refname";
     };
   };
 }

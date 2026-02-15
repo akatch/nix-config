05d297a21cea333ab8194dc7b6de4f8e6feb88bd
Al Bowles <bowlesan@proton.me>
2026-02-15 14:18:01 -0600

    fix(vault): Mount as user

--- nixos/modules/vault/default.nix
+++ nixos/modules/vault/default.nix
@@ -12,7 +12,7 @@
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,users,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      automount_opts = "x-systemd.automount,users,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=1000";
 
     in ["${automount_opts},credentials=/etc/nixos/smb"];
   };

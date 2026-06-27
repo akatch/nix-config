46f2be250ad2f8dad025a2b11cf792745e009f73
Al Bowles <bowlesan@proton.me>
2026-06-26 19:12:50 -0500

    feat: Enable and disable laptop display when opening and closing the lid

--- home-manager/hosts/europa.nix
+++ home-manager/hosts/europa.nix
@@ -31,6 +31,8 @@
     ''
       bindgesture swipe:right workspace prev
       bindgesture swipe:left workspace next
+      bindswitch --locked lid:on output DP-2 enable; output DP-2 pos 0 2160; output eDP-1 disable; workspace 1 output DP-2; workspace 2 output DP-2
+      bindswitch --locked lid:off output eDP-1 enable; workspace 1 output eDP-1; workspace 2 output eDP-1 #when lid opens enable screen
       bindsym Mod4+z output DP-2 enable; output DP-2 pos 0 2160; output eDP-1 disable; workspace 1 output DP-2; workspace 2 output DP-2
       bindsym Mod4+Shift+z output eDP-1 enable; workspace 1 output eDP-1; workspace 2 output eDP-1
       default_border pixel 1
